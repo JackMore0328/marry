f215a591db36a404b48ac56846eb22c255a782ad
jackmore0328 <jackmore0328@163.com>
2019-01-28 14:40:12 +0800

    修改订单字段

--- admin/src/main/java/com/door/match/dao/OrderDao.java
+++ admin/src/main/java/com/door/match/dao/OrderDao.java
@@ -1,5 +1,6 @@
 package com.door.match.dao;
 
+import com.door.match.entity.PayPO;
 import com.door.match.entity.SysOrder;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,10 +10,10 @@ import java.util.List;
 @Mapper
 public interface OrderDao {
 
-    SysOrder findSysOrderById(@Param("id") Long id);
+    PayPO findSysOrderById(@Param("id") Long id);
 
-    List<SysOrder> listSysOrder(SysOrder user);
+    List<PayPO> listSysOrder(PayPO user);
 
-    long countSysOrder(SysOrder user);
+    long countSysOrder(PayPO user);
 
 }
